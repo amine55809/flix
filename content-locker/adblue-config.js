6fda39cb62d969fadfc64651434be9f9f2990e44
hassanamehry <137705716+hassanamehry@users.noreply.github.com>
2025-11-06 20:48:54 +0100

    first commit

---
+++ content-locker/adblue-config.js
@@ -0,0 +1,17 @@
+/**
+ * AdBlueMedia API Configuration
+ * This file contains sensitive API credentials
+ * DO NOT commit this file to version control
+ */
+
+window.ADBLUE_CONFIG = {
+    userId: '321842',
+    apiKey: 'cfe031daa224cb470f9d4c9a7a531982',
+    offerFeedUrl: 'https://d2xohqmdyl2cj3.cloudfront.net/public/offers/feed.php',
+    checkLeadsUrl: 'https://d2xohqmdyl2cj3.cloudfront.net/public/external/check2.php',
+    numOffers: 3,
+    requiredOffers: 1,
+    checkInterval: 15000,
+    testing: 0
+};
+
